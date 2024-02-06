0405fd80296a30110cd79586a1a97c41cc805b7d
Mi-Seun <misun.jang73@gmail.com>
2024-02-07 00:27:44 +0100

    ajout la section compétence

--- script/main.js
+++ script/main.js
@@ -31,38 +31,51 @@ document.querySelectorAll('.nav a').forEach(link => {
 
 
 /* ///////////////////////////////////// */
-/* /////// Part of Expériences ///////// */
-/* //////////////////////////////////// */
+/* ////////// Part of skills /////////// */
+/* ///////////////////////////////////// */
 
-// Creating a new Leaflet map
+
+
+
+
+
+
+
+
+
+/* ///////////////////////////////////// */
+/* /////// Part of Expériences ///////// */
+/* ///////////////////////////////////// */
+
+// Création d'une nouvelle carte Leaflet
 let map = L.map('map', {
-center: [26.65341, 62.226563], // Coordinates for Lyon [lat, long]
-zoom: 2
+    center: [26.65341, 62.226563], // Coordonnées pour Lyon [lat, long]
+    zoom: 2
 });
 
-// Adding background map layers for Lyon
+// Ajout des couches de fond de carte pour Lyon
 let baselayers = {
-CartoDB: L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'),
-OSM: L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png'),
-Esri: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'),
-LyonMap: L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png') // Lyon background map
+    CartoDB: L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'),
+    OSM: L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png'),
+    Esri: L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'),
+    LyonMap: L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png') // Carte de fond de Lyon
 };
-baselayers.CartoDB.addTo(map); // Using Lyon background map
+baselayers.CartoDB.addTo(map); // Utilisation de la carte de fond de Lyon
 
-// Adding scale to the map
+// Ajout de l'échelle à la carte
 L.control.scale({maxWidth: 100}).addTo(map);
 
-// Function to create custom icons
+// Fonction pour créer des icônes personnalisées
 function createCustomIcon(iconUrl) {
-return L.icon({
-    iconUrl: iconUrl,
-    iconSize: [52, 72], // Icon size
-    iconAnchor: [16, 32], // Icon anchor point
-    popupAnchor: [0, -32] // Popup position relative to the icon
-});
+    return L.icon({
+        iconUrl: iconUrl,
+        iconSize: [52, 72], // Taille de l'icône
+        iconAnchor: [16, 32], // Point d'ancrage de l'icône
+        popupAnchor: [0, -32] // Position de la popup par rapport à l'icône
+    });
 }
 
-// Creating different icons
+// Création de différentes icônes
 let developpeurIcon = createCustomIcon('asset/images/icon/icon_dev.png');
 let restaurantIcon = createCustomIcon('asset/images/icon/icon_restaurant.png');
 let ongIcon = createCustomIcon('asset/images/icon/icon_ong.png');
@@ -72,68 +85,51 @@ let consultingIcon = createCustomIcon('asset/images/icon/icon_consulting.png');
 let associationIcon = createCustomIcon('asset/images/icon/icon_association.png');
 let designIcon = createCustomIcon('asset/images/icon/icon_design.png');
 
-// Function to add markers to the map with popups
+// Fonction pour ajouter des marqueurs à la carte avec des popups
 function addMarkerToMap(coordinates, icon, popupContent, zoomLevel, markerId) {
-let marker = L.marker(coordinates, { icon: icon }).addTo(map);
-marker.bindPopup(popupContent).openPopup();
-marker.on('click', function () {
-    console.log("Marker clicked");
-    map.setView(coordinates, zoomLevel);
-    console.log("Popup Content: ", popupContent);
-    updatePopUpContentForMarker(markerId);
-});
-return marker;
-}
 
-document.addEventListener('DOMContentLoaded', function () {
-// Add a class to your <a> tags to make them easier to target
-document.querySelectorAll('.popUp-map a').forEach(function (link) {
-    link.addEventListener('click', function (event) {
-    event.preventDefault();
-    let markerId = this.getAttribute('data-marker');
-    updatePopUpContentForMarker(markerId);
-    // Open the popUp-map
-    $('.popUp-map').show();
+    let marker = L.marker(coordinates, { icon: icon }).addTo(map);
+    marker.bindPopup(popupContent).openPopup();
+    marker.on('click', function () {
+        console.log("Marqueur cliqué");
+        map.setView(coordinates, zoomLevel);
+        console.log("Contenu de la popup : ", popupContent);
+        updatePopUpContentForMarker(markerId);
     });
-});
-});
-
-function updatePopUpContentForMarker(markerId) {
-let popUpMapDiv = document.querySelector('.popUp-map p');
-let markerContent = getMarkerContent(markerId);
-popUpMapDiv.innerHTML = markerContent;
+    return marker;
 }
 
-// Adding markers with different icons and popups
-let marker1 = addMarkerToMap([45.7381, 4.8331], developpeurIcon, "<b>Web Developer, Ice development, Lyon<br><br>2023:<br>- Create websites<br>- WordPress</b>", 13, 'marker1');
-let marker2 = addMarkerToMap([45.7645, 4.8292], restaurantIcon, "<b>Restaurant Manager, Minanée<br><br>2007-2013:<br>- </b>", 13);
-let marker3 = addMarkerToMap([45.7717, 4.8511], restaurantIcon, "<b>Restaurant Manager, Sushido<br><br>2002-2007:<br>- </b>", 13);
-let marker4 = addMarkerToMap([45.752643, 4.829307], designIcon, "<b>Designe<br><br>2022:<br>- Create website, logo, menu card<br>- Consulting restaurant management</b>", 13);
-let marker5 = addMarkerToMap([48.019324, -1.604004], consultingIcon, "<b>Consulting Restaurant Management, Séoul to go, Rennes<br><br>2022:<br>- Create website, logo, menu card<br>- Consulting restaurant management</b>", 13);
-let marker6 = addMarkerToMap([45.767792, 4.835991], managementIcon, "<b>President of the Association of Lyon Koreans, European Representative, Event Organizer, Lyon<br><br>2013:<br>- </b>", 13);
-let markerAfrica = addMarkerToMap([-30.5595, 22.9375], ongIcon, "<b>Volunteer Charity NGO, South Africa<br><br>2014-2019:<br>- Team Leader, Social Aid, Cook</b>", 13);
-let markerLondon = addMarkerToMap([51.5074, -0.1278], ongIcon, "<b>Volunteer</b>", 13);
-let markerSeoul = addMarkerToMap([37.5665, 126.9780], ongIcon, "<b>Secretary, KIMM (Korea Institute of Machinery & Materials)</b>", 13);
-let markerMonde = addMarkerToMap([-23.885838, 126.5625], globalIcon, "<b>KOWWINER: Owner of international businesses, Kowiner</b>", 13);
-let markerEurope = addMarkerToMap([48.136767, 16.435547], globalIcon, "<b>European Representative</b>", 13);
+// Ajout de marqueurs avec différentes icônes et popups
+let marker1 = addMarkerToMap([45.7381, 4.8331], developpeurIcon, "<b>Développeur Web, Ice développement, Lyon<br><br>2023:<br>- Création de sites web<br>- WordPress</b>", 13, 'marker1');
+let marker2 = addMarkerToMap([45.7645, 4.8292], restaurantIcon, "<b>Gérant de restaurant, Minanée<br><br>2007-2013:<br>- </b>", 13);
+let marker3 = addMarkerToMap([45.7717, 4.8511], restaurantIcon, "<b>Gérant de restaurant, Sushido<br><br>2002-2007:<br>- </b>", 13);
+let marker4 = addMarkerToMap([45.752643, 4.829307], designIcon, "<b>Design<br><br>2022:<br>- Création de site web, logo, carte de menu<br>- Conseil en gestion de restaurant</b>", 13);
+let marker5 = addMarkerToMap([48.019324, -1.604004], consultingIcon, "<b>Consultant en gestion de restaurant, Séoul to go, Rennes<br><br>2022:<br>- Création de site web, logo, carte de menu<br>- Conseil en gestion de restaurant</b>", 13);
+let marker6 = addMarkerToMap([45.767792, 4.835991], managementIcon, "<b>Président de l'Association des Coréens de Lyon, Représentant européen, Organisateur d'événements, Lyon<br><br>2013:<br>- </b>", 13);
+let markerAfrica = addMarkerToMap([-30.5595, 22.9375], ongIcon, "<b>ONG de bénévolat caritatif, Afrique du Sud<br><br>2014-2019:<br>- Chef d'équipe, Aide sociale, Cuisinier</b>", 13);
+let markerLondon = addMarkerToMap([51.5074, -0.1278], ongIcon, "<b>Bénévole</b>", 13);
+let markerSeoul = addMarkerToMap([37.5665, 126.9780], ongIcon, "<b>Secrétaire, KIMM (Institut coréen de machines et de matériaux)</b>", 13);
+let markerMonde = addMarkerToMap([-23.885838, 126.5625], globalIcon, "<b>KOWWINER : Propriétaire d'entreprises internationales, Kowiner</b>", 13);
+let markerEurope = addMarkerToMap([48.136767, 16.435547], globalIcon, "<b>Représentant européen</b>", 13);
 
-// Standalone popup to display "View my experiences"
+// Popup autonome pour afficher "Voir mes expériences"
 let standalonePopup = L.popup()
-.setLatLng([57.891497, 61.171875])
-.setContent("View my experiences")
-.openOn(map);
+    .setLatLng([57.891497, 61.171875])
+    .setContent("Voir mes expériences")
+    .openOn(map);
 
-// Layer control for map elements
+// Contrôle de couche pour les éléments de la carte
 L.control.layers(baselayers, null, { position: 'topright', collapsed: false }).addTo(map);
 L.control.layers(null, {
-"Europe": L.layerGroup([marker1, marker2, marker3, marker4, marker5, marker6, markerEurope, markerLondon]),
-"Africa": L.layerGroup([markerAfrica]),
-"Asia": L.layerGroup([markerSeoul]),
-"World": L.layerGroup([markerMonde])
+    "Europe": L.layerGroup([marker1, marker2, marker3, marker4, marker5, marker6, markerEurope, markerLondon]),
+    "Afrique": L.layerGroup([markerAfrica]),
+    "Asie": L.layerGroup([markerSeoul]),
+    "Monde": L.layerGroup([markerMonde])
 }, {
-position: 'bottomright',
-collapsed: false
+    position: 'bottomright',
+    collapsed: false
 }).addTo(map);
+
 
 
 
@@ -189,101 +185,36 @@ function topFunction() {
 
 
 
-// Répétez le modèle ci-dessus pour les autres marqueurs
 
 
 
-// Function to add markers to the map with popups
-// function addMarkerToMap(coordinates, icon, popupContent, zoomLevel) {
-//     let marker = L.marker(coordinates, { icon: icon }).addTo(map);
-//     marker.bindPopup(popupContent).openPopup();
-//     marker.on('click', function() {
-//         map.setView(coordinates, zoomLevel);
-//         updatePopUpContent(popupContent);
-//     });
-//     return marker;
-// }
+/* ///////////////////////////////////// 
 
-// function getMarkerContent(markerId) {
-//     switch (markerId) {
-//         case 'marker1':
-//             return "<b>Web Developer, Ice development, Lyon<br><br>2023:<br>- Create websites<br>- WordPress</b>";
-//         case 'marker2':
-//             return "<b>Web Developer, Ice development, Lyon<br><br>2023:<br>- Create websites<br>- WordPress</b>";
-//         default:
-//             return "Default content";
-//     }
-// }
+document.addEventListener('DOMContentLoaded', function () {
+// Add a class to your <a> tags to make them easier to target
+document.querySelectorAll('.popUp-map a').forEach(function (link) {
+    link.addEventListener('click', function (event) {
+    event.preventDefault();
+    let markerId = this.getAttribute('data-marker');
+    updatePopUpContentForMarker(markerId);
+    // Open the popUp-map
+    $('.popUp-map').show();
+    });
+});
+});
 
-
-
-// Searching for HTML elements
-// let popUpMapDiv = document.querySelector('.popUp-map p');
-
-// Function to update the popup content
-// function updatePopUpContent(htmlContent) {
-//     let popUpMapDiv = document.querySelector('.popUp-map p');
-//     popUpMapDiv.innerHTML = htmlContent;
-//     $('.popUp-map').show();
-// }
-
-// Handling a click on the map to hide the popup element
-// map.on('click', function() {
-//     $('.popUp-map').hide();
-// });
+function updatePopUpContentForMarker(markerId) {
+let popUpMapDiv = document.querySelector('.popUp-map p');
+let markerContent = getMarkerContent(markerId);
+popUpMapDiv.innerHTML = markerContent;
+}
+*/
 
 
 
 
 
 
-
-
-
-
-
-// Handling a click on a marker to show the corresponding popup content
-// marker1.on('click', function () {
-//     console.log("Marker 1 clicked");
-//     updatePopUpContent("<b>Web Developer, Ice development, Lyon<br><br>2023:<br>- Create websites<br>- WordPress</b>");
-// });
-
-
-// marker2.on('click', function() {
-//     updatePopUpContent("<b>Restaurant Manager, Minanée<br><br>2007-2013:<br>- </b>");
-// });
-// marker3.on('click', function() {
-//     updatePopUpContent("<b>Restaurant Manager, Sushido<br><br>2002-2007:<br>- </b>");
-// });
-// marker4.on('click', function() {
-//     updatePopUpContent("<b>Design<br><br>2022:<br>- Infographiste <br>- </b>");
-// });
-// marker5.on('click', function() {
-//     updatePopUpContent("<b>Consulting Restaurant Management, Séoul to go, Rennes<br><br>2022:<br>- Create website, logo, menu card<br>- Consulting restaurant management</b>");
-// });
-// marker6.on('click', function() {
-//     updatePopUpContent("<b>Organization<br><br>2013:<br>- </b>");
-// });
-// markerAfrica.on('click', function() {
-//     updatePopUpContent("<b>Volunteer Charity NGO, South Africa<br><br>2014-2019:<br>- Team Leader, Social Aid, Cook</b>");
-// });
-// markerLondon.on('click', function() {
-//     updatePopUpContent("<b>Volunteer</b>");
-// });
-// markerSeoul.on('click', function() {
-//     updatePopUpContent("<b>Volunteer, Séoul</b>");
-// });
-// markerMonde.on('click', function() {
-//     updatePopUpContent("<b>2007-2013, 2022 Infographist-Design Creating paper communication materials & Website, photography restaurants Minanée & Séoul to go, Lyon & Rennes</b>");
-// });
-// markerEurope.on('click', function() {
-//     updatePopUpContent("<b>2007-2013, 2022 Infographist-Design Creating paper communication materials & Website, photography restaurants Minanée & Séoul to go, Lyon & Rennes</b>");
-// });
-
-// Handling a click on the map to hide the popup element
-// map.on('click', function() {
-//     $('.popUp-map').hide();
-// });
 
 
 
